25eadeaa45811e8f68c6f253bce9fa04a76778d0
Emre Kocer <emreke6@gmail.com>
2024-11-20 20:28:52 +0300

    problematic synthesize for logq=60 , new wlm_mixed, nonstd int_mul

--- src/bu/modsub.v
+++ src/bu/modsub.v
@@ -1,7 +1,7 @@
 
 
 module modsub#(
-            parameter LOGQ                  =       64,
+            parameter LOGQ                  =       60,
             parameter Reduc_param           =       17
         )
         (
